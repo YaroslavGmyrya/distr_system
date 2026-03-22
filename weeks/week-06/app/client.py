8fb4131bf78f31e51e653ea44469bee91c8d49d7
YaroslavGmyrya <jjaked25@gmail.com>
2026-03-22 19:18:59 +0700

    add week 6

--- weeks/week-06/app/client.py
+++ weeks/week-06/app/client.py
@@ -1,4 +1,5 @@
 # Реализуйте здесь клиент для GraphQL.
+import requests
 
 def build_payload(query: str, variables: dict) -> dict:
     """
@@ -9,4 +10,47 @@ def build_payload(query: str, variables: dict) -> dict:
     :return: Словарь с ключами "query" и "variables".
     """
     # Ваш код здесь
-    raise NotImplementedError
+    return {"query": query, "variables": variables}
+
+URL = "http://127.0.0.1:8000/graphql"
+
+query = """
+query GetOrders {
+  orders {
+    id
+    product
+    quantity
+  }
+}
+"""
+
+query2 = """
+mutation CreateOrder($product: String!, $quantity: Int!) {
+  createOrder(product: $product, quantity: $quantity) {
+    id
+    product
+    quantity
+  }
+}
+"""
+
+request_1 = build_payload(query=query, variables={})
+request_2 = build_payload(query=query2, variables={"product":"book", "quantity":200})
+
+
+response = requests.post(URL, json=request_1)
+
+print("BEFORE REQUEST:\n",response.content)
+print("\n\n")
+
+
+response = requests.post(URL, json=request_2)
+
+print("REQUEST ANSWER:\n",response.content)
+print("\n\n")
+
+response = requests.post(URL, json=request_1)
+
+print("AFTER REQUEST:\n",response.content)
+print("\n\n")
+
