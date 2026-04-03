4ae1a5a0975397cfd472bb51727b0d5fe09236a9
YaroslavGmyrya <jjaked25@gmail.com>
2026-04-03 16:21:24 +0700

    update

--- weeks/week-07/starter/service.py
+++ weeks/week-07/starter/service.py
@@ -1,19 +1,31 @@
 import grpc
 from concurrent import futures
-# Импортируйте сгенерированные модули
-# import service_pb2
-# import service_pb2_grpc
 
-class ServiceImplementation: # Унаследуйтесь от сгенерированного Servicer
-    pass
-    # Реализуйте методы сервиса
+import service_pb2
+import service_pb2_grpc
+
+
+class ServiceImplementation(service_pb2_grpc.TasksServiceServicer):
+    def MyMethod(self, request, context):
+        
+        response_text = f"Получен id: {request.id}"
+        return service_pb2.MyResponse(result=response_text)
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # service_pb2_grpc.add_MyServiceServicer_to_server(ServiceImplementation(), server)
+
+    service_pb2_grpc.add_TasksServiceServicer_to_server(
+        ServiceImplementation(), server
+    )
+
     server.add_insecure_port('[::]:50051')
     server.start()
+
+    print("gRPC server started on port 50051")
+
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
